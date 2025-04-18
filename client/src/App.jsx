41ead363c6274b92c21b4a9f49f63677c9bd1786
20eg112151 <20eg112151@anurag.edu.in>
2025-04-18 19:32:24 +0530

    Fix server build with package.json and dependencies

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,1 +1,12 @@
 // Placeholder content for App.jsx
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Welcome to TrendFlicks</h1>
+    </div>
+  );
+};
+
+export default App;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,1 +1,6 @@
 // Placeholder content for main.jsx
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
